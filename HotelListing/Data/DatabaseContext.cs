4f93810ca75db690599bac702e4171e7bb691bf2
Dulanjana <Dulanjana@DESKTOP-4MSIP85>
2023-05-06 16:40:28 +0530

    Added Database, AutoMapper, Unit Of Work and DTOs

---
+++ HotelListing/Data/DatabaseContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {}
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Sri Lanka",
+                    ShortName = "SL"
+                }
+            );
+
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sundals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Ratings = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort suits",
+                    Address = "George Town",
+                    CountryId = 2,
+                    Ratings = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Colombo",
+                    CountryId = 3,
+                    Ratings = 4
+                }
+            );
+        }
+    }
+}
